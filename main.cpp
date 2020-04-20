33bcaa50dc019973141de902ee7b6ffe9e8f2b99
Martin Dimitrov <donmartineze3@gmail.com>
2020-04-20 16:40:41 +0300

    Paused console after a played game

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "BlackJack.hpp"
-
+#include <iostream>
 //Github repo https://github.com/martiii1/Project_BlackJack_FN45711
+//Martin Dimitrov FN45711 group6
 
 int main()
 {
     BlackJack Game;
 
     Game.Start();
+
+
+    std::cin.get();
+    std::cout << "\n \n \nPress any key to exit! ";
+    std::cin.get();
 
     return 0;
 }
